5de6753615349f770344fbbf4a9f57079104a368
Татьяна <robsten9086@mail.ru>
2023-07-09 17:39:40 +0300

    задача 10

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите трёхзначное число: ");
+        int number = int.Parse(Console.ReadLine());
+
+        int secondDigit = (number / 10) % 10;
+
+        Console.WriteLine("Вторая цифра числа: " + secondDigit);
+    }
+}
